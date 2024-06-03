a581041ef07964ef2015cdf8b6f1c1a755680578
rafael <162385496+rrafaleao@users.noreply.github.com>
2024-06-03 18:48:52 +0000

    Trabalho finalizado!

--- main.cpp
+++ main.cpp
@@ -17,12 +17,6 @@ struct Tarefa {
     string titulo;
 };
 
-// Protótipos das funções
-void addTarefa(vector<Tarefa> &tarefas);
-void visualizarTarefa(const vector<Tarefa> &tarefas);
-void editarTarefa(vector<Tarefa> &tarefas);
-void apagarTarefa(vector<Tarefa> &tarefas);
-
 void addTarefa(vector<Tarefa> &tarefas) {
     Tarefa tarefaNova;
 
@@ -35,18 +29,18 @@ void addTarefa(vector<Tarefa> &tarefas) {
     cout << "Digite o " << ANSI_COLOR_GREEN << "Título" << ANSI_COLOR_RESET << " da Tarefa: ";
     getline(cin, tarefaNova.titulo);
 
+    cout << "Digite uma " << ANSI_COLOR_GREEN << "Descrição " << ANSI_COLOR_RESET << "para a tarefa: ";
+    getline(cin, tarefaNova.descricao);
+
     cout << "Digite a data de " << ANSI_COLOR_GREEN << "vencimento" << ANSI_COLOR_RESET << " da tarefa conforme o exemplo(25/05/2024): ";
     getline(cin, tarefaNova.vencimento);
 
     cout << "Digite o " << ANSI_COLOR_GREEN << "Status" << ANSI_COLOR_RESET << " da Tarefa (" << ANSI_COLOR_RED << "1 - Pendente" << ANSI_COLOR_RESET << ", " << ANSI_COLOR_YELLOW << "2 - Em Progresso" << ANSI_COLOR_RESET << ", " << ANSI_COLOR_GREEN << "3 - Concluída" << ANSI_COLOR_RESET << "): ";
     getline(cin, tarefaNova.status);
 
-    cout << "Digite uma " << ANSI_COLOR_GREEN << "Descrição " << ANSI_COLOR_RESET << "para a tarefa: ";
-    getline(cin, tarefaNova.descricao);
+    cout << ANSI_COLOR_GREEN << "Tarefa Adicionada Com Sucesso!!" << ANSI_COLOR_RESET << endl;
 
     tarefas.push_back(tarefaNova);
-
-    cout << ANSI_COLOR_GREEN << "Tarefa Adicionada Com Sucesso!!" << ANSI_COLOR_RESET << endl;
 }
 
 void visualizarTarefa(const vector<Tarefa> &tarefas) {
@@ -88,14 +82,14 @@ void editarTarefa(vector<Tarefa> &tarefas) {
             cout << "Digite o novo" << ANSI_COLOR_GREEN << " título" ANSI_COLOR_RESET " da tarefa: ";
             getline(cin, tarefa.titulo);
 
+            cout << "Digite a nova" <<ANSI_COLOR_GREEN<<" descrição"<<ANSI_COLOR_RESET<<" da tarefa: ";
+            getline(cin, tarefa.descricao);
+
             cout << "Digite a nova data de" <<ANSI_COLOR_GREEN<< " vencimento"<< ANSI_COLOR_RESET <<" da tarefa (conforme o exemplo: 25/05/2024): ";
             getline(cin, tarefa.vencimento);
 
             cout << "Digite o novo" <<ANSI_COLOR_GREEN<<" status" <<ANSI_COLOR_RESET << " da tarefa (1 - Pendente, 2 - Em Progresso, 3 - Concluída): ";
             getline(cin, tarefa.status);
-
-            cout << "Digite a nova" <<ANSI_COLOR_GREEN<<" descrição"<<ANSI_COLOR_RESET<<" da tarefa: ";
-            getline(cin, tarefa.descricao);
 
             cout << ANSI_COLOR_GREEN "Tarefa editada com sucesso!" ANSI_COLOR_RESET << endl;
             break;
@@ -110,9 +104,62 @@ void editarTarefa(vector<Tarefa> &tarefas) {
 void apagarTarefa(vector<Tarefa> &tarefas){
     string apagarId;
     cout<<"Você escolheu Apagar Tarefa."<<endl;
-    cout<<"Digite o ID da tarefa que quer Apagar: "<<endl;
+    cout<<"Digite o" <<ANSI_COLOR_GREEN<< " ID"<<ANSI_COLOR_RESET<< " da tarefa que quer"<<ANSI_COLOR_RED " Apagar: "<<ANSI_COLOR_RESET;
     cin>>apagarId;
 
+    for (size_t i = 0; i < tarefas.size(); ++i) {
+    	if (tarefas[i].id == apagarId) {
+        	tarefas.erase(tarefas.begin() + i);
+            cout<<ANSI_COLOR_GREEN<<"Tarefa Apagada com sucesso! "<<ANSI_COLOR_RESET<<endl;
+        	break;
+        }
+    }
+}
+
+void buscaTarefa(const vector<Tarefa> &tarefas){
+    string buscaTitulo;
+
+    cout<<"Você escolheu buscar tarefa!"<<endl;
+    cin.ignore();
+    cout<<"Digite o"<<ANSI_COLOR_GREEN<<" TITULO"<<ANSI_COLOR_RESET<< " da tarefa que está buscando: ";
+    getline(cin,buscaTitulo);
+
+    bool encontrada = false;
+    for (const auto& tarefa : tarefas) {
+    if (tarefa.titulo == buscaTitulo) {
+        encontrada = true;
+        cout <<"Tarefas encontradas: " <<endl;
+        cout <<ANSI_COLOR_GREEN "ID: "<<ANSI_COLOR_RESET <<tarefa.id <<", ";
+        cout <<ANSI_COLOR_GREEN "Título: "<<ANSI_COLOR_RESET <<tarefa.titulo << ", ";
+        cout <<ANSI_COLOR_GREEN "Descriçao: "<<ANSI_COLOR_RESET <<tarefa.descricao<< ", ";
+        cout <<ANSI_COLOR_GREEN "Vencimento: "<<ANSI_COLOR_RESET <<tarefa.vencimento<< ", ";
+        cout <<ANSI_COLOR_GREEN "Status: "<<ANSI_COLOR_RESET <<tarefa.status<< ", ";
+        break;
+      } 
+    }
+
+}
+
+void filtrarStatus(vector<Tarefa> &tarefas){
+    string filtrarStatus;
+    cout<<"Você escolheu Filtar tarefa por status! "<<endl;
+    cin.ignore();
+    cout<<"Digite o status para filtrar (1 - Pendente, 2 - Em Progresso, 3 - Concluido): "<<endl;
+    cin>>filtrarStatus;
+
+    bool encontrada = false;
+    for (const auto& tarefa : tarefas) {
+    if (tarefa.status == filtrarStatus) {
+        encontrada = true;
+        cout <<"Tarefas encontradas: " <<endl;
+        cout <<ANSI_COLOR_GREEN "ID: "<<ANSI_COLOR_RESET <<tarefa.id <<", ";
+        cout <<ANSI_COLOR_GREEN "Título: "<<ANSI_COLOR_RESET <<tarefa.titulo << ", ";
+        cout <<ANSI_COLOR_GREEN "Descriçao: "<<ANSI_COLOR_RESET <<tarefa.descricao<< ", ";
+        cout <<ANSI_COLOR_GREEN "Vencimento: "<<ANSI_COLOR_RESET <<tarefa.vencimento<< ", ";
+        cout <<ANSI_COLOR_GREEN "Status: "<<ANSI_COLOR_RESET <<tarefa.status<< ", ";
+        
+      } 
+    }
 }
 
 int main() {
@@ -144,8 +191,13 @@ int main() {
                 editarTarefa(tarefas);
                 break;
             case 4:
+                apagarTarefa(tarefas);
+                break;
             case 5:
+                buscaTarefa(tarefas);
+                break;
             case 6:
+                filtrarStatus(tarefas);
             default:
                 cout << ANSI_COLOR_RED << "ERRO, insira uma opção válida: " << ANSI_COLOR_RESET << endl;
         }
