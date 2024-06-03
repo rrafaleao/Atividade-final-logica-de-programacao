d4ee698a4f81256f785e16da96845ea185333496
rafael <162385496+rrafaleao@users.noreply.github.com>
2024-06-03 22:08:53 +0000

    Trabalho Finalizado

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
-
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
